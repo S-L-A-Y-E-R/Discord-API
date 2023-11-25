34401222939369608b48c5c91e9c92b53656d7c4
ahmed <elshehry@gamil.com>
2023-11-25 19:30:54 +0200

    Integrated member endpoints

---
+++ src/routes/memberRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getAllMembers,
+  getMember,
+  createMember,
+  updateMember,
+  deleteMember,
+} from "../controllers/memberController";
+
+const router = express.Router();
+
+router.route("/").get(getAllMembers).post(createMember);
+
+router.route("/:id").get(getMember).patch(updateMember).delete(deleteMember);
+
+export default router;
