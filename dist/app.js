770f78922add603d3a06a79eaaa781af9879fc27
ahmed <elshehry@gamil.com>
2023-12-01 14:38:33 +0200

    Added the production host to the CORS whitelist

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.server = exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -9,21 +11,29 @@ const path_1 = __importDefault(require("path"));
 const http_1 = require("http");
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
-const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
+const express_mongo_sanitize_1 = __importDefault(
+  require("express-mongo-sanitize")
+);
 const xss_1 = __importDefault(require("xss"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const compression_1 = __importDefault(require("compression"));
 const appError_1 = __importDefault(require("./utils/appError"));
-const errorController_1 = __importDefault(require("./controllers/errorController"));
+const errorController_1 = __importDefault(
+  require("./controllers/errorController")
+);
 const profileRoutes_1 = __importDefault(require("./routes/profileRoutes"));
 const serverRoutes_1 = __importDefault(require("./routes/serverRoutes"));
 const memberRoutes_1 = __importDefault(require("./routes/memberRoutes"));
 const channelRoutes_1 = __importDefault(require("./routes/channelRoutes"));
-const conversationRoutes_1 = __importDefault(require("./routes/conversationRoutes"));
+const conversationRoutes_1 = __importDefault(
+  require("./routes/conversationRoutes")
+);
 const socketIo_1 = __importDefault(require("./utils/socketIo"));
 const messageRoutes_1 = __importDefault(require("./routes/messageRoutes"));
-const directMessagesRoutes_1 = __importDefault(require("./routes/directMessagesRoutes"));
+const directMessagesRoutes_1 = __importDefault(
+  require("./routes/directMessagesRoutes")
+);
 const app = (0, express_1.default)();
 exports.app = app;
 // Socket.io setup
@@ -31,8 +41,8 @@ const server = (0, http_1.createServer)(app);
 exports.server = server;
 const io = (0, socketIo_1.default)(server);
 app.use((req, res, next) => {
-    req.io = io;
-    return next();
+  req.io = io;
+  return next();
 });
 //Set security http headers
 app.use((0, helmet_1.default)());
@@ -44,13 +54,19 @@ app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 //Enable outsource proxies
 app.set("trust proxy", true);
 //Allow cors for all domains
-app.use((0, cors_1.default)({
-    origin: ["https://admin.socket.io", "http://localhost:3000"],
+app.use(
+  (0, cors_1.default)({
+    origin: [
+      "https://admin.socket.io",
+      "http://localhost:3000",
+      "https://discord-website-production.up.railway.app/",
+    ],
     credentials: true,
-}));
+  })
+);
 //Use morgan logger in the develpment
 if (process.env.NODE_ENV === "development")
-    app.use((0, morgan_1.default)("dev"));
+  app.use((0, morgan_1.default)("dev"));
 //Data sanitization agains noSQL query injection
 app.use((0, express_mongo_sanitize_1.default)());
 //Data sanitization against xss attacks
@@ -59,7 +75,7 @@ app.use((0, express_mongo_sanitize_1.default)());
 app.use((0, cookie_parser_1.default)());
 //Compress all text sent in the response to the client
 if (process.env.NODE_ENV === "production") {
-    app.use((0, compression_1.default)());
+  app.use((0, compression_1.default)());
 }
 //Global resources
 app.use("/api/v1/profiles", profileRoutes_1.default);
@@ -71,11 +87,13 @@ app.use("/api/v1/messages", messageRoutes_1.default);
 app.use("/api/v1/direct-messages", directMessagesRoutes_1.default);
 // Handle requests from wrong urls
 app.all("*", (req, res, next) => {
-    next(new appError_1.default(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(
+    new appError_1.default(`Can't find ${req.originalUrl} on this server!`, 404)
+  );
 });
 //Using global error handling middleware
 app.use(errorController_1.default);
 //Socket connection route
 app.use("/api/v1/socket", (req, res) => {
-    res.status(200).json({ message: "Socket.io connection route" });
+  res.status(200).json({ message: "Socket.io connection route" });
 });
